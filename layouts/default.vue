48b3da58ad19ab1683e6dde2679240798b303f61
INFJakZda <zdanowski78@gmail.com>
2018-12-01 23:26:16 +0100

    Add header components, and to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,31 @@
 <template>
   <div>
+    <TheHeader 
+      @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false" />
     <nuxt/>
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+
+export default {
+  data() {
+    return {
+      displaySidenav: false
+    }
+  },
+  components: {
+    TheHeader,
+    TheSidenav
+  }
+}
+</script>
+
 
 <style>
 html {
