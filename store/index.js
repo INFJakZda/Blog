7616195655c83b0605e740a168a652b26a83ea69
INFJakZda <zdanowski78@gmail.com>
2018-12-05 18:58:24 +0100

    Add axios get in nuxtServerInit in store

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,27 +13,16 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit("setPosts", [
-              {
-                id: "1",
-                title: "First Post",
-                previewText: "This is our first post!",
-                thumbnail:
-                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-              },
-              {
-                id: "2",
-                title: "Second Post",
-                previewText: "This is our second post!",
-                thumbnail:
-                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-              }
-            ]);
-            resolve();
-          }, 1000);
-        });
+        return axios
+          .get("https://nuxt-blog-68b5f.firebaseio.com/posts.json")
+          .then(res => {
+            const postArray = [];
+            for (const key in res.data) {
+              postArray.push({ ...res.data[key], id: key });
+            }
+            vuexContext.commit("setPosts", postArray);
+          })
+          .catch();
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
